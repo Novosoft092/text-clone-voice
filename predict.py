6e2d9cd8138a21088f69dd78840b04c830bdc32a
fahad nasar <statbdigarai@gmail.com>
2026-07-31 00:19:46 +0530

    Enable offline mode for Hugging Face Transformers
    Set environment variables for offline mode in Hugging Face Transformers.

--- predict.py
+++ predict.py
@@ -1,12 +1,13 @@
 from cog import BasePredictor, Input, Path
+import os
+os.environ["HF_HUB_OFFLINE"] = "1"
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
 import soundfile as sf
 import numpy as np
 from transformers import AutoModel
-
 class Predictor(BasePredictor):
   def setup(self):
     self.model = AutoModel.from_pretrained("ai4bharat/IndicF5", trust_remote_code=True)
-
   def predict(self, text: str = Input(description="Text to speak"), ref_audio: Path = Input(description="Reference speaker audio wav"), ref_text: str = Input(description="Reference audio transcript")) -> Path:
     wav = self.model(text, ref_audio_path=str(ref_audio), ref_text=ref_text)
     out = "/tmp/out.wav"
